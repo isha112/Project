9d2dd68537419a12d3d5b59090c94f13bcad36a0
isha112 <shah.is@husky.neu.edu>
2015-11-03 15:23:39 -0500

    First commit
    Basic structure

---
+++ public/index.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module("mainApp", ["ngRoute", "ui.bootstrap"]);
+
+app.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/home', {
+            templateUrl: 'projectPages/home/home.html',
+            controller: 'HomeController'
+
+        }).
+       otherwise({
+           redirectTo: '/home'
+       });
+  }]);
---
+++ public/projectPages/home/home.js
@@ -0,0 +1,24 @@
+﻿app.controller("HomeController", function ($scope, $http, $routeParams) {
+
+    //autocomplete
+    $(function () {
+        var input = document.getElementById('cityname');
+        var autocomplete = new google.maps.places.Autocomplete(input);
+    });
+
+    //req API for events
+    $scope.getEvents = function () {
+        
+        var x = document.getElementById('cityname').value;
+        $scope.name = x.substr(0, x.indexOf(','));
+        //alert($scope.name);
+        $scope.events = [];
+
+        $http.jsonp("http://api.eventful.com/jsonp/events/search?q=tag:jazz&l=" + $scope.name + "&app_key=ncZMbJNvDKFCTLR3" + "&callback=JSON_CALLBACK")
+        .success(function (result) {
+            console.log(result.events.event);
+            $scope.events = result.events.event;
+
+        })
+    }
+});
