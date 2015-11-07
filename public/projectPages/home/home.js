76987f0e4a63057181321494e6363e3464187032
isha112 <isha112@ccs.neu.edu>
2015-11-07 07:52:44 -0500

    UI changes

--- public/projectPages/home/home.js
+++ public/projectPages/home/home.js
@@ -6,7 +6,7 @@
         var autocomplete = new google.maps.places.Autocomplete(input);
 
         $('#demo').pinterest_grid({
-            no_colums: 4,
+            no_colums: 3,
             padding_x: 10,
             padding_y: 10,
             margin_bottom: 50,
@@ -21,9 +21,10 @@
         $scope.name = x.substr(0, x.indexOf(','));
         //alert($scope.name);
         $scope.events = [];
-
-        $http.jsonp("http://api.eventful.com/jsonp/events/search?q=tag:jazz&l=" + $scope.name + "&app_key=ncZMbJNvDKFCTLR3" + "&callback=JSON_CALLBACK")
+        
+        $http.jsonp("http://api.eventful.com/jsonp/events/search?q=jazz&l=" + $scope.name + "&image_sizes=block150,large&app_key=ncZMbJNvDKFCTLR3" + "&callback=JSON_CALLBACK")
         .success(function (result) {
+            $scope.altimg = '../../public/images/events-alt.jpg';
             $scope.events = result.events.event;
 
         })
